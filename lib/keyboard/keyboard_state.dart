c601b78ed992f6b776e17d8a529fcce3915db4e8
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-21 18:54:57 +0530

    Update keyboard_state.dart

--- lib/keyboard/keyboard_state.dart
+++ lib/keyboard/keyboard_state.dart
@@ -8,17 +8,12 @@ class KeyboardState {
   bool isShiftOn;
 
   KeyboardState({
-    this.language = KeyboardLanguage.urdu,
+    this.language = KeyboardLanguage.english,
     this.isShiftOn = false,
   });
 
   void toggleLanguage() {
-    language = language == KeyboardLanguage.urdu
-        ? KeyboardLanguage.english
-        : KeyboardLanguage.urdu;
-  }
-
-  void toggleShift() {
-    isShiftOn = !isShiftOn;
+    language =
+        language == KeyboardLanguage.urdu ? KeyboardLanguage.english : KeyboardLanguage.urdu;
   }
 }
