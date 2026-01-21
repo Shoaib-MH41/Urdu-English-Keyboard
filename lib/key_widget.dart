ece155a21e8ce6642cb1cbf84d565506278d7eeb
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-21 18:43:29 +0530

    Update key_widget.dart

--- lib/key_widget.dart
+++ lib/key_widget.dart
@@ -2,23 +2,31 @@ import 'package:flutter/material.dart';
 
 class KeyWidget extends StatelessWidget {
   final String label;
-  final VoidCallback? onTap;
+  final VoidCallback onTap;
+  final double flex;
 
-  const KeyWidget({super.key, required this.label, this.onTap});
+  const KeyWidget({
+    super.key,
+    required this.label,
+    required this.onTap,
+    this.flex = 1,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: flex.toInt(),
       child: Padding(
         padding: const EdgeInsets.all(4),
-        child: GestureDetector(
+        child: InkWell(
           onTap: onTap,
+          borderRadius: BorderRadius.circular(8),
           child: Container(
             height: 48,
             alignment: Alignment.center,
             decoration: BoxDecoration(
-              color: Colors.grey.shade800,
-              borderRadius: BorderRadius.circular(10),
+              color: Colors.grey.shade300,
+              borderRadius: BorderRadius.circular(8),
             ),
             child: Text(
               label,
