a6ebd93ffb6a5d282bdd0d3c49154b15c5e83b6e
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-21 18:52:00 +0530

    Update english.dart

--- lib/layouts/english.dart
+++ lib/layouts/english.dart
@@ -2,43 +2,42 @@ import 'package:flutter/material.dart';
 import '../key_widget.dart';
 
 class EnglishKeyboard extends StatelessWidget {
+  final Function(String) onKeyTap;
+  final VoidCallback onBackspace;
+  final VoidCallback onSpace;
   final VoidCallback onGlobePressed;
 
   const EnglishKeyboard({
     super.key,
+    required this.onKeyTap,
+    required this.onBackspace,
+    required this.onSpace,
     required this.onGlobePressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey.shade200,
-      padding: const EdgeInsets.symmetric(vertical: 6),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-
-          _row(["q","w","e","r","t","y","u","i","o","p"]),
-          _row(["a","s","d","f","g","h","j","k","l"]),
-          _row(["z","x","c","v","b","n","m"]),
-
-          /// 🔹 bottom row
-          Row(
-            children: [
-              KeyWidget(label: "🌐", onTap: onGlobePressed, flex: 1.5),
-              KeyWidget(label: "space", onTap: () {}, flex: 4),
-              KeyWidget(label: "⌫", onTap: () {}, flex: 1.5),
-            ],
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        _row(["q","w","e","r","t","y","u","i","o","p"]),
+        _row(["a","s","d","f","g","h","j","k","l"]),
+        _row(["z","x","c","v","b","n","m"]),
+        Row(
+          children: [
+            KeyWidget(label: "🌐", onTap: onGlobePressed, flex: 1.5),
+            KeyWidget(label: "space", onTap: onSpace, flex: 4),
+            KeyWidget(label: "⌫", onTap: onBackspace, flex: 1.5),
+          ],
+        ),
+      ],
     );
   }
 
   Widget _row(List<String> keys) {
     return Row(
       children: keys
-          .map((k) => KeyWidget(label: k, onTap: () {}))
+          .map((k) => KeyWidget(label: k, onTap: () => onKeyTap(k)))
           .toList(),
     );
   }
