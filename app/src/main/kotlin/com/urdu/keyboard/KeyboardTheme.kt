36991385c2849a4c77fb5d3cadf4b6c67cac07c3
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-22 07:03:45 +0530

    Create KeyboardTheme.kt

---
+++ app/src/main/kotlin/com/urdu/keyboard/KeyboardTheme.kt
@@ -0,0 +1,10 @@
+package com.urdu.keyboard
+
+import android.graphics.Color
+
+object KeyboardTheme {
+    val darkBackground = Color.parseColor("#121212")
+    val keyColor = Color.parseColor("#2C2C2E")
+    val accentColor = Color.parseColor("#00ADB5")
+    val textColor = Color.WHITE
+}
