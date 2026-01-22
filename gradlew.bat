c511167edc980c8bc6a15f5375bbbed3c1e81a20
Shoaib-MH41 <princeshoaib2122@gmail.com>
2026-01-22 06:11:17 +0530

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@rem Gradle Wrapper script for Windows
+@if "%DEBUG%" == "" @echo off
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
